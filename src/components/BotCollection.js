62539268db2e3ee99c142afdd5e4ed60b211861d
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-08-12 01:10:14 +0300

    thirdcommit

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import BotCard from './BotCard';
 
-function BotCollection() {
+const BotCollection = ({ addBotToArmy }) => {
+  const [bots, setBots] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://bot-battr-json-server-five.vercel.app/bots')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => setBots(data))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div>BotCollection</div>
-  )
-}
+    <div>
+      <h2>Bot Collection</h2>
+      <div className="bot-collection">
+        {bots.map(bot => (
+          <BotCard key={bot.id} bot={bot} handleClick={() => addBotToArmy(bot)} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default BotCollection
+export default BotCollection;
